48b2da988644410f748b1dd9e6a2dd2001a98bdc
FlorianHoy <florian.hoyaux@outlook.fr>
2024-03-22 09:48:36 +0000

    Update view.py

--- flask_docker/view.py
+++ flask_docker/view.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template
 import os
+from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
+metrics = PrometheusMetrics(app)
 
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return "Test12345"
 
 
 if __name__ == "__main__":
